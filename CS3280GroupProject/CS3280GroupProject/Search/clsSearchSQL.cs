938041e52f2526b202d615c6c840cca44d988030
Unknown <tayloralvey@mail.weber.edu>
2018-07-26 14:59:56 -0600

    Search SQL nearly finished, but not tested

--- CS3280GroupProject/CS3280GroupProject/Search/clsSearchSQL.cs
+++ CS3280GroupProject/CS3280GroupProject/Search/clsSearchSQL.cs
@@ -9,7 +9,8 @@ namespace CS3280GroupProject.Search
 {
     public static class clsSearchSQL
     {
-        #region Method
+        #region Methods
+        #region CBSQL
         /// <summary>
         /// This SQL gets all invoices
         /// </summary>
@@ -29,6 +30,48 @@ namespace CS3280GroupProject.Search
         }
 
         /// <summary>
+        /// This SQL gets all InvoiceDates
+        /// </summary>
+        /// <returns>All InvoiceDates</returns>
+        public static string GetAllInvoiceDates()//Not tested
+        {
+            try
+            {
+                string sSQL = "SELECT InvoiceDate FROM Invoices";
+                return sSQL;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " ->" + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// This SQL gets all invoice total costs
+        /// </summary>
+        /// <returns>All invoice totals</returns>
+        public static string GetAllInvoiceTotals()//Not tested
+        {
+            try
+            {
+                string sSQL = "SELECT SUM(id.Cost) " +
+                              "FROM id ItemDesc, li LineItems, i Invoices" +
+                              "WHERE i.InvoiceNum = li.InvoiceNum " +
+                              "AND li.ItemCode = id.ItemCode" +
+                              "GROUP BY i.InvoiceNum";
+                return sSQL;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " ->" + ex.Message);
+            }
+        }
+        #endregion
+
+        #region DGFiltersSQL
+        /// <summary>
         /// This SQL gets all data on an invoice for a given InvoiceNum.
         /// </summary>
         /// <param name="sInvoiceNum">The InvoiceNum for the invoice to retrieve all data.</param>
@@ -47,6 +90,52 @@ namespace CS3280GroupProject.Search
             }
         }
 
+        /// <summary>
+        /// This SQL gets all invoices with a given InvoiceDate
+        /// </summary>
+        /// <param name="sInvoiceDate">The InvoiceDate for the invoice to retrieve all data.</param>
+        /// <returns>All invoices for the given InvoiceDate.</returns>
+        public static string GetAllInvoicesWithDate(string sInvoiceDate)//Not tested
+        {
+            try
+            {
+                string sSQL = "SELECT * FROM Invoices WHERE InvoiceDate = " + sInvoiceDate;
+                return sSQL;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " ->" + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// This SQL gets the total cost of an invoice for a given InvoiceNum
+        /// </summary>
+        /// <param name="sInvoiceNum">The InvoiceNum for the invoice to retrieve the total cost.</param>
+        /// <returns>The total cost of the invoice.</returns>
+        public static string GetAllInvoicesWithTotal(string sInvoiceNum)//Not complete Not tested
+        {
+            try
+            {
+                string sSQL = "SELECT InvoiceNum " +
+                              "FROM " +
+                                  "(SELECT i.InvoiceNum, SUM(id.Cost) " +
+                                  "FROM id ItemDesc, li LineItems, i Invoices" +
+                                  "WHERE i.InvoiceNum = li.InvoiceNum " +
+                                  "AND li.ItemCode = id.ItemCode" +
+                                  "GROUP BY i.InvoiceNum)";
+                return sSQL;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " ->" + ex.Message);
+            }
+        }
+        #endregion
+
+        #region OtherSQL - Possibly Not Needed
         /// <summary>
         /// This SQL gets the date for an invoice for a given InvoiceNum
         /// </summary>
@@ -75,7 +164,7 @@ namespace CS3280GroupProject.Search
         {
             try
             {
-                string sSQL = "SELECT i.InvoiceNum, SUM(id.Cost) " +
+                string sSQL = "SELECT SUM(id.Cost) " +
                               "FROM id ItemDesc, li LineItems, i Invoices" +
                               "WHERE i.InvoiceNum = li.InvoiceNum " +
                               "AND li.ItemCode = id.ItemCode" +
@@ -89,6 +178,7 @@ namespace CS3280GroupProject.Search
                                     MethodInfo.GetCurrentMethod().Name + " ->" + ex.Message);
             }
         }
+        #endregion
         #endregion
     }
 }
