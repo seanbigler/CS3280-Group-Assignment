87b83c2f65a26a6fa9f1e98463ec959af4962538
Sean Bigler <seanbigler@mail.weber.edu>
2018-07-27 15:49:46 -0600

    Added comments within action methods

--- CS3280GroupProject/CS3280GroupProject/Items/wndItem.xaml.cs
+++ CS3280GroupProject/CS3280GroupProject/Items/wndItem.xaml.cs
@@ -24,5 +24,43 @@ namespace CS3280GroupProject.Items
         {
             InitializeComponent();
         }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            /*Take contents of txtCode, txtDescription, and txtCost and insert it into the
+             * ItemDesc table.
+             * 
+             * Error checking to make sure all boxes have input, all inputs are valid, and that
+             * contents of txtCode does not already exist as a key in ItemDesc
+             */
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            /*Deletes selected item (selected on the datagrid) from the ItemDesc table
+             * 
+             * Error checking to make sure a row is selected and selected item is not on any
+             * existing invoice
+             */
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            /*Updates selected item (selected on the datagrid) in the ItemDesc table
+             * based on contents of txtDescription and txtCost
+             * 
+             * Will only update Description and Cost. Values in txtCode will be ignored
+             * 
+             * Error checking to make sure a valid row is selected, all boxes (txtDescription and txtCost) 
+             * have input, and that all inputs are valid
+             */
+        }
+
+        private void dgItems_CurrentCellChanged(object sender, EventArgs e)
+        {
+            /*When cell is changed, text boxes will fill with corresponding data from
+             * selected row
+             */
+        }
     }
 }
